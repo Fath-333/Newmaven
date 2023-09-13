e24a7c7b5277b8f65b36278f849952e385a71e7c
Fathima Sharafudeen <Fathima Sharafudeen@DESKTOP-02GK4BS.mshome.net>
2023-09-13 14:16:55 +0530

    New project

---
+++ Mavenprojects/src/main/java/Javaprograms/Addition.java
@@ -0,0 +1,13 @@
+package Javaprograms;
+
+public class Addition {
+	
+		public static void main(String args[])
+		{
+		int a = 5;
+		int b = 4;
+		int sum = a+b;
+	System.out.println(sum);	
+		}
+		}
+
